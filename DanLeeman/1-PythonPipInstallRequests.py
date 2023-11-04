332cd2c18a48099fd0098c713af7aae6a94c375e
julienPalleau <jpalleau@gmail.com>
2023-11-04 21:15:41 +0100

    added 1-PythonPipInstallRequests, 2-PythonRequestsGetAndPostRequests, 3-PythonRequestsUploadAFile, 4-PythonRequestsJson

---
+++ DanLeeman/1-PythonPipInstallRequests.py
@@ -0,0 +1,1 @@
+# https://www.youtube.com/watch?v=pEI6tQV6hsE&list=PLJ1odve0o6dX5ndJ5lwiCOR58ycB1rcrV&index=1
---
+++ DanLeeman/4-PythonRequestsJson.py
@@ -0,0 +1,1 @@
+# https://www.youtube.com/watch?v=zZU0JZmIvKA&list=PLJ1odve0o6dX5ndJ5lwiCOR58ycB1rcrV&index=4
