82a8d43bf6ec94dbb5d9ee9392b37f703e26e0f4
julienPalleau <jpalleau@gmail.com>
2023-11-06 10:06:18 +0100

    Begining Chapter 8 PythonRequestsRedirection

--- DanLeeman/1-PythonPipInstallRequests.py
+++ DanLeeman/1-PythonPipInstallRequests.py
@@ -1,1 +1,5 @@
 # https://www.youtube.com/watch?v=pEI6tQV6hsE&list=PLJ1odve0o6dX5ndJ5lwiCOR58ycB1rcrV&index=1
+
+###############################
+# Python Pip Install Requests #
+##############################3
---
+++ DanLeeman/8-PythonRequestsRedirection.py
@@ -0,0 +1,5 @@
+# https://www.youtube.com/watch?v=lfqG4q8hVew&list=PLJ1odve0o6dX5ndJ5lwiCOR58ycB1rcrV&index=8
+
+###############################
+# Python Requests Redirection #
+###############################
