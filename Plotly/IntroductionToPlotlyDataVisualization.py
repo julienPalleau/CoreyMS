7692eb277c1eaefb986848ebfc7344f493e6c864
julienPalleau <jpalleau@gmail.com>
2023-11-17 13:48:55 +0100

    sync

--- Plotly/IntroductionToPlotlyDataVisualization.py
+++ Plotly/IntroductionToPlotlyDataVisualization.py
@@ -1,1 +1,19 @@
 # https://www.youtube.com/watch?v=_b2KXL0wHQg
+"""
+1. How to create any chart you want using plotly documentation: 3 steps that I use for creating powerful graphs & charts with Plotly:
+    a. Initiate your graph with Plotly sample code, and adapt it to your data
+        (https://plotly.com/python)
+    b. Enhance your graph with Plotly Express High-level interface
+        (https://plotly.com/python-api-reference/plotly.express.html)
+    c. Customize your graphs with Plotly Graph_Objects, using fig.Updates
+        (https://plotly.com/python/reference/index/)
+            i. Layout represents the chart (frames, title, color, tick, hover, legend)
+            ii. Traces are representing the data (inside the layout)
+
+    d.
+            i. update_traces
+            ii. update_layout
+            iii. update_annotations
+            iv. update_yaxes
+            v. update_xaxes
+"""
